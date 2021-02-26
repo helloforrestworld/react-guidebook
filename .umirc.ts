a82d9c10ac685a866316e896919d09771aad0694
刘富炜 <fuwei.liu@happy-seed.com>
2021-02-26 15:30:45 +0800

    chore: modify publicPath

--- .umirc.ts
+++ .umirc.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'Algorithm GuideBook',
   mode: 'site',
+  base: '/algorithm-guidebook',
+  publicPath: '/algorithm-guidebook/',
+  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   // more config: https://d.umijs.org/config
 });
